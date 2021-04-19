dfac34f2c0829c201f37409c3c932ebc5b20255b
Kim Dong-hyeon <66898812+ms0130k@users.noreply.github.com>
2021-04-20 02:19:36 +0900

    'update'

---
+++ p_2-1.py
@@ -0,0 +1,5 @@
+from math import pi 
+
+x = input()
+area = round(pi * (float(x) ** 2), 1)
+print('The area of a circle with radius', float(x), 'is', area)
---
+++ p_2-3.py
@@ -0,0 +1,6 @@
+#print('''Fahrenheit to Celsius conversion
+#Degrees in Fahrenheit?''', end=' ')
+x = input('''Fahrenheit to Celsius conversion
+Degrees in Fahrenheit? ''')
+print(round((float(x) - 32) * 5 / 9, 1), 'degrees in Celsius')
+
